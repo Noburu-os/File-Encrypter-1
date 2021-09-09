a8e4dedfe515b492f34c1b1fcc65317c560c80ea
Noburu-os <Jusin.driskell@outlook.com>
2021-09-09 14:27:36 -0500

    Update cryptcode.py
    you're welcome

--- cryptcode.py
+++ cryptcode.py
@@ -1,4 +1,9 @@
-fo = open("subject.png","rb") #rb reads binary
+#small encrypter
+import hashlib
+
+fo = open("hsw.js","rb")#rb reads binary
+
+file_name = input('File Name:')#input a file name so the program know what to encrypt
 image = fo.read()
 image = bytearray(image)
 key = 48 # Valuws accepted bw 0 - 255
@@ -7,3 +12,11 @@ for index, value in enumerate(image):
 fo = open("encrypted_subject.png","wb")
 fo.write(image)
 fo.close()
+#the things up above are not needed for a encrypter, check out my multi encrypyer on my profile @Noburu-OS
+
+
+#######starting a small simple hashlib encrypter######
+wrd_enc = file_name.encode('utf-8')
+digested = hashlib.md5(wrd_enc.strip()).hexdigest()
+print('encryption started{}...')
+print('file encrypted:' + (digested))
